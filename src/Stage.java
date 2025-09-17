2c8790a0e8c95276cbc88c678cc29b5cc6fc3521
xaipersad <xaishan.persad@students.mq.edu.au>
2025-09-18 01:20:46 +1000

    actors randomly spawn

--- src/Stage.java
+++ src/Stage.java
@@ -4,17 +4,30 @@ import java.awt.Point;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.Random;
 
 public class Stage {
   Grid grid;
   List<Actor> actors;
 
   public Stage() {
-    grid = new Grid();
-    actors = new ArrayList<Actor>();
-    actors.add(new Cat(grid.cellAtColRow(0, 0).get()));
-    actors.add(new Dog(grid.cellAtColRow(0, 15).get()));
-    actors.add(new Bird(grid.cellAtColRow(12, 9).get()));    
+  grid = new Grid();
+  actors = new ArrayList<Actor>();
+  Random rand = new Random();
+  int x1 = rand.nextInt(20);
+    int y1 = rand.nextInt(20);
+    int x2, y2, x3, y3;
+    do {
+      x2 = rand.nextInt(20);
+      y2 = rand.nextInt(20);
+    } while (x2 == x1 && y2 == y1);
+    do {
+      x3 = rand.nextInt(20);
+      y3 = rand.nextInt(20);
+    } while ((x3 == x1 && y3 == y1) || (x3 == x2 && y3 == y2));
+    actors.add(new Cat(grid.cellAtColRow(x1, y1).get()));
+    actors.add(new Dog(grid.cellAtColRow(x2, y2).get()));
+    actors.add(new Bird(grid.cellAtColRow(x3, y3).get()));
   }
 
   public void paint(Graphics g, Point mouseLoc) {
